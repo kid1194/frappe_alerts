95d573203f2c6bdc3bfc3cbac23e26724b9dcbd8
kid1194 <kid1194@gmail.com>
2024-02-17 01:20:59 +0300

    v1.0.2 release

---
+++ alerts/setup/uninstall.py
@@ -0,0 +1,50 @@
+# Alerts © 2024
+# Author:  Ameen Ahmed
+# Company: Level Up Marketing & Software Development Services
+# Licence: Please refer to LICENSE file
+
+
+import frappe
+
+
+# [Hooks, Install]
+def after_uninstall():
+    from frappe.model.delete_doc import delete_doc
+    
+    docs = [
+        ["DocType", [
+            "Alert Seen By",
+            "Alert For Role",
+            "Alert For User",
+            "Alerts Update Receiver",
+            "Alert",
+            "Alert Type",
+            "Alerts Settings"
+        ]],
+        ["Module Def", ["Alerts"]]
+    ]
+    
+    try:
+        doc = frappe.qb.DocType("Notification Log")
+        (
+            frappe.qb.from_(doc)
+            .delete()
+            .where(doc.document_type.isin(docs[0][1]))
+        ).run()
+    except Exception:
+        pass
+    
+    for doc in docs:
+        for name in doc[1]:
+            try:
+                delete_doc(
+                    doc[0], name,
+                    ignore_permissions=True,
+                    ignore_missing=True,
+                    ignore_on_trash=True,
+                    delete_permanently=True
+                )
+            except Exception:
+                pass
+    
+    frappe.clear_cache()
